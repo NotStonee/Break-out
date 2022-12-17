3b49a031c36d2b99c200e908973ba4e89a9ad816
Stonee <119343829+NotStonee@users.noreply.github.com>
2022-12-17 00:50:48 +0000

    animation frame update

--- script.js
+++ script.js
@@ -2,6 +2,11 @@
     var score = 0;
     var lives = 5;
     var gameOver = false;
+  function update() {
+    // update game state
+    // draw game
+    requestAnimationFrame(update);
+  }
 
 const canvas = document.getElementById('game');
     const context = canvas.getContext('2d');
@@ -224,25 +229,23 @@ const canvas = document.getElementById('game');
     }
   }
 }
-    // listen to keyboard events to move the paddle
     document.addEventListener('keydown', function(e) {
-      // left arrow key
-      if (e.which == 65 && gameOver == false) {
-        paddle.dx = -7;
-      }
+  if (gameOver) return;
 
-      if (e.which == 37 && gameOver == false) {
-        paddle.dx = -7;
-      }
-      // right arrow key
-      if (e.which == 68 && gameOver == false) {
-        paddle.dx = 7;
-      }
+  switch (e.which) {
+    case 37: // left arrow key
+    case 65: // 'A' key
+      paddle.dx = -7;
+      requestAnimationFrame(update);
+      break;
+    case 39: // right arrow key
+    case 68: // 'D' key
+      paddle.dx = 7;
+      requestAnimationFrame(update);
+      break;
+  }
 
-      if (e.which == 39 && gameOver == false) {
-        paddle.dx = 7;
-      }
-
+    document.addEventListener('keydown', function(e) {
       if (e.which == 82 && gameOver == true) {
         lives = 5;
         score = 0;
@@ -251,11 +254,13 @@ const canvas = document.getElementById('game');
         paddle.x = canvas.width / 2 - brickWidth / 2;
         paddle.y = 440;
       }
+    });
       
       // space key
       // if they ball is not moving, we can launch the ball using the space key. ball
       // will move towards the bottom right to start
       if (lives > 0 && ball.dx == 0 && ball.dy == 0 && e.which == 32) {
+        event.preventDefault();
         ball.dx = ball.speed;
         ball.dy = ball.speed;
         if (chance < 5) {
@@ -264,19 +269,14 @@ const canvas = document.getElementById('game');
       }
     });
     
-    // listen to keyboard events to stop the paddle if key is released
-    document.addEventListener('keyup', function(e) {
-      if (e.which == 65 || e.which == 37) {
+        // listen to keyboard events to stop the paddle if key is released
+        document.addEventListener('keyup', function(e) {
+      if (e.which == 65 || e.which == 37 || e.which == 68 || e.which == 39) {
         paddle.dx = 0;
+        requestAnimationFrame(update);
       }
     });
 
-    document.addEventListener('keyup', function(e) {
-      if(e.which == 68 || e.which == 39) {
-        paddle.dx = 0
-      }
-      
-    })
 
     function text(txt, fnt, x, y, c) {
       context.fillStyle = c;
